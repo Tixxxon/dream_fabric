010526bcdf27e5c10816375d2ff057af45f34251
fenixsarov <fenixsarov@gmail.com>
2017-10-19 21:08:43 +0300

    Added packeges to package.json, created env-var in gulp-config

--- src/app.js
+++ src/app.js
@@ -3,15 +3,28 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
+var config = require('config');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var products = require('./routes/products');
+// var index = require('./routes/index');
+// var products = require('./routes/products');
 
+// Create app and configure this
 var app = express();
+//app.set('port', config.get('port'));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/templates');
 app.set('view engine', 'pug');
+
+// Setup logger
+if (app.get('env') == 'development') {
+    app.use(logger('dev')); // immidiate - ключ для отправки в лог до начала запроса
+} else {
+    app.use(logger('default'));
+}
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,14 +33,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/products', products);
+// app.use('/', index);
+// app.use('/products', products);
+
+
+// Middlewares
+app.use(function (req, res, next) {
+    if (req.url == '/') {
+        res.end("Hello, Express!");
+    } else {
+        next();
+    }
+});
+
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 // error handler
