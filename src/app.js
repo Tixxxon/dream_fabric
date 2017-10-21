c5fecb08d5f86e665a688f7eda5fbf7e1d0327c7
fenixsarov <fenixsarov@gmail.com>
2017-10-21 23:46:21 +0300

    Minimal fix

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ if (app.get('env') == 'development') {
 }
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public/images/', 'favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
